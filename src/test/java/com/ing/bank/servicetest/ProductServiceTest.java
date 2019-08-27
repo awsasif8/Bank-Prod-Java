d53913c04ee90820020bd9f0c72059e4920e9fa5
sharathgs <sharathgs777@gmail.com>
2019-08-27 15:00:41 +0530

    test for services

--- src/test/java/com/ing/bank/servicetest/ProductServiceTest.java
+++ src/test/java/com/ing/bank/servicetest/ProductServiceTest.java
@@ -2,7 +2,9 @@ package com.ing.bank.servicetest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,8 +12,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.ing.bank.dto.ProductDescriptionResponseDto;
 import com.ing.bank.dto.ProductNameDto;
 import com.ing.bank.dto.ProductNameResponeDto;
+import com.ing.bank.entity.Product;
 import com.ing.bank.repository.ProductNameRepository;
 import com.ing.bank.service.ProductNameServiceImpl;
 
@@ -53,10 +57,45 @@ public class ProductServiceTest {
 		return productDto;
 	}
 	
+	public Product getProduct()
+	{
+		Product product = new Product();
+		product.setProductDescription("Test for this type");
+		product.setProductId(1);
+		product.setProductName("Test");
+		product.setProductType("Insurance");
+		return product;
+	}
+	
+	public Product getProduct1()
+	{
+		Product product = new Product();
+		product.setProductDescription("Test for this type");
+		product.setProductId(2);
+		product.setProductName("Test2");
+		product.setProductType("Health Insurance");
+		return product;
+	}
+	
+	
 	@Test
 	public void productNamePositive()
 	{
-		Mockito.when(productRepository.findAll());
+		List<Product> addProducts = new ArrayList<Product>();
+		addProducts.add(getProduct());
+		addProducts.add(getProduct1());
+		Mockito.when(productRepository.findAll()).thenReturn(addProducts);
+		ProductNameResponeDto productName = productService.getProductNameList();
+		Assert.assertEquals(200, productName.getStatusCode());
+	}
+	
+	@Test
+	public void productDescription()
+	{
+		Mockito.when(productRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(getProduct()));
+		ProductDescriptionResponseDto productDescription = productService.getProductDescription(Mockito.anyInt());
+		Assert.assertEquals(200, productDescription.getStatusCode());
+		
 	}
 
 }
